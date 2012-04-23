f3cc4b7df199da8cf71606b42c8ea3daf775f38b
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-04-22 20:09:26 -0400

    fixed timing error

--- autonomous_vehicle.ino
+++ autonomous_vehicle.ino
@@ -16,7 +16,7 @@ int highVolts;
 int startVolts;
 byte Charged=1;                                               // 0=Flat battery  1=Charged battery
 
-unsigned long kill = millis();
+unsigned long permission = 10000UL;
 boolean move = false;
 int LeftPWM = 100;                                                  // PWM value for left  motor speed / brake
 int RightPWM = 100;                                                 // PWM value for right motor speed / brake
@@ -68,14 +68,14 @@ void loop()
 }
 
 void calculate_movement(){
-  float m_heading = gps.course();
+  float m_heading = gps.f_course();
   float t_heading = TinyGPS::course_to( mlat, mlon, tlat, tlon);
   float diff = t_heading - m_heading;
   
   diff += (diff>180) ? -360 : (diff<-180) ? 360 : 0;    // http://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
 
-  LeftPWM = (diff > 10) ? 200 : (diff < -10) ? 100 : 200;
-  RightPWM = (diff > 10) ? 100 : (diff < -10) ? 200 : 200;
+  LeftPWM = (diff > 10) ? 200 : (diff < -10) ? 150 : 220;
+  RightPWM = (diff > 10) ? 150 : (diff < -10) ? 200 : 220;
 /*  
   if(diff > 10){
     LeftPWM = 200;
@@ -90,7 +90,7 @@ void calculate_movement(){
   */
   
   
-  //Serial.println(diff, 5);
+  //Serial.println(int(diff));
 }
 
 void processSerial(){
@@ -99,23 +99,21 @@ void processSerial(){
     if(aChar == '\n') {
       // End of message detected. Time to parse
       if(String(inData).indexOf("!") != -1){
-        kill = millis();
-        move = true;
+        permission = 1UL;
       } else {
        
         char *p = inData; 
         char *str;
       
-        int counter = 0; 
+        boolean first = true; 
            
         while (str = strtok_r(p, ",", &p)){
-          if(counter == 0){
+          if(first){
             tlat = atof(str);
-            counter = 1;
           } else {
             tlon = atof(str);
-            counter = 0;
           }
+          first = !first;
         } 
       
         //Serial.print("TLat: ");
@@ -136,42 +134,45 @@ void processSerial(){
 }
 
 void get_gps(){
-  bool newData = false;
-
   // For one second we parse GPS data and report some key values
-  for (unsigned long start = millis(); millis() - start < 500;)
+  for (unsigned long start = millis(); millis() - start < 500UL;)
   {
     while (ss.available()) // characters in the buffer
     {
       char c = ss.read();
        //Serial.write(c ); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) // Did a new valid sentence come in?
-        newData = true;
+      if (gps.encode(c)){ // Did a new valid sentence come in?
+        unsigned long age;
+        gps.f_get_position(&mlat, &mlon, &age);
+        
+        //Serial.print(mylat, 10);
+        //Serial.print(",");
+        //Serial.println(mylon,10);
+      }
     }
-  }
-
-  if (newData)
-  {
-    unsigned long age;
-    gps.f_get_position(&mlat, &mlon, &age);
-    
-    //Serial.print(mylat, 10);
-    //Serial.print(",");
-    //Serial.println(mylon,10);
   }
 }
 
 void move_vehicle(){
   //----------------------------------------------------------- GOOD BATTERY speed controller opperates normally ----------------------
   // --------------------------------------------------------- Code to drive dual "H" bridges --------------------------------------
+  permission = permission + 1UL;
 
-  if (move && millis() - kill < 1000){                                           // Only power motors if battery voltage is good
+  if ( permission < 5UL ){         // Only power motors if battery voltage is good
+    //Serial.println(permission);
     if ((millis()-leftoverload)>overloadtime){  // if left motor has not overloaded recently        
       l_motor(0, LeftPWM);                      // left motor forward
     }
     if ((millis()-rightoverload)>overloadtime){ // if right motor has not overloaded recently
       r_motor(0, RightPWM);                     // right motor forward
     } 
+  } else{
+    l_motor(0, 0);
+    r_motor(0, 0);
+  }
+  
+  if( permission > 32000UL){
+    permission = 1500UL;
   }
   /*
   else{                                        // Battery is flat
@@ -194,7 +195,7 @@ void r_motor(int a_pwm, int b_pwm){
 void voltage_check(){
   //-------- Check battery voltage and current draw of motors ------
   
-  //Volts=analogRead(Battery);                                  // read the battery voltage
+  Volts=analogRead(Battery);                                  // read the battery voltage
   LeftAmps=analogRead(LmotorC);                               // read left motor current draw
   RightAmps=analogRead(RmotorC);                              // read right motor current draw
   //Serial.print(LeftAmps);Serial.print("    ");Serial.println(RightAmps); 
@@ -225,7 +226,7 @@ void voltage_check(){
     chargeTimer=millis();                                     // record the time
 
     //digitalWrite (Charger,0);                                 // enable current regulator to charge battery
-    Serial.println("NEEDS CHARGING!");
+    //Serial.println("NEEDS CHARGING!");
   }
   
 }
