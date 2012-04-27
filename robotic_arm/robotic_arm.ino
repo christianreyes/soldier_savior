12829c27914f5482468332274c58f541415393e8
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-04-26 20:19:40 -0400

    arm code updated

--- robotic_arm/robotic_arm.ino
+++ robotic_arm/robotic_arm.ino
@@ -1,20 +1,39 @@
-#include <SoftwareSerial.h>
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
 
-SoftwareSerial ss(2, 3);
 
-const int killswitch = 7;
-
-void setup()
-{
-  pinMode(killswitch, INPUT);
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+Servo two;               // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(5);  // attaches the servo on pin 9 to the servo object 
+  two.attach(6);
+  
   Serial.begin(9600);
-  ss.begin(4800);
-  Serial.println("0.0,0.0");
-}
-
-// skeleton code
-void loop()
-{
-  if(digitalRead(killswitch) == HIGH)
-    Serial.println("!");
+  Serial.println("ARM ONLINE");
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(5);    
+    two.write(pos);
+        // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(5); 
+ two.write(pos);
+        // waits 15ms for the servo to reach the position 
+  } 
 }
